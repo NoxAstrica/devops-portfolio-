65b3c4314878f99e9725d180cf831540801cd9b2
hope <DESKTOP-HOPE>
2025-10-21 23:26:49 +0300

    check

--- source/good_code.py
+++
@@ -1,8 +0,0 @@
-"""Good code example for lint testing."""
-
-def greet(name):
-    """Return a greeting for the given name."""
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    print(greet("World"))
